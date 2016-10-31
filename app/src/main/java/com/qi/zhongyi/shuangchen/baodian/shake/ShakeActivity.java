40c0389d92fec7297effeec2f19944068162f085
qihao <yugu88@126.com>
2016-10-31 22:41:53 +0800

    调整摇一摇界面

--- app/src/main/java/com/qi/zhongyi/shuangchen/baodian/shake/ShakeActivity.java
+++ app/src/main/java/com/qi/zhongyi/shuangchen/baodian/shake/ShakeActivity.java
@@ -9,7 +9,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.RelativeLayout;
 import android.widget.Toast;
-
 import com.qi.zhongyi.shuangchen.baodian.R;
 import com.qi.zhongyi.shuangchen.baodian.utils.ActivityUtil;
 
